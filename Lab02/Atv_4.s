21de82bb0cf28fe787d312a53b756ec567a19e37
ogaitns022 <tiagons022@gmail.com>
2026-03-17 15:42:16 -0300

    Ler até o \0

--- Lab02/Atv_4.s
+++ Lab02/Atv_4.s
@@ -1,13 +1,13 @@
-addi x11, x0, 32 
-addi x12, x11, 12
+addi x11, x0, 28
 
 loop: 
-blt x12, x11, fim 
 lb x10, 0(x11)
+beq x10, x0, fim
 sb x10, 1024(x0)
 addi x11, x11, 1
-beq x0, x0, loop
+jal x0, loop
 
 fim:
 halt
+
 str1: .string "Hello World"
